d7495c6a6615683e5ea514db735283310b6fee70
Jessany Kaline <jessany42@gmail.com>
2022-09-25 20:45:37 -0300

    inclusão da página de compras
    Inclusão da páginas de compras

---
+++ comprar/functions.js
@@ -0,0 +1,55 @@
+/*const cep = document.querySelector('#cep');
+
+cep.addEventListener("blur", (e) => {
+  let search = cep.value.replace("-", "");
+  console.log(search);
+ 
+  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+  .then(response => {
+      response.json()
+      .then(data => {
+          console.log(data)
+          showData(data)
+      })
+  })
+  .catch(e => {
+      console.log("ERRO: "+e)
+  })
+})
+
+const showData = (result) => {
+  for (const campo in result){
+      if(document.querySelector("#endereco"+campo)){
+          document.querySelector("#endereco"+campo).value = result[campo];
+      }
+  }
+}*/
+
+const options = {
+   method: 'GET',
+   mode: 'no-cors',
+   cache: 'default'
+}
+
+
+
+
+function buscaCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json`, options)
+      .then (response => response.json())
+      .then (dados => {
+        console.log(dados)
+  
+        document.querySelector("#endereco").value = dados.logradouro;
+        document.querySelector("#cidade").value = dados.localidade;
+        document.querySelector("#uf").value = dados.uf;
+  
+        /*document.querySelector("#txtNumero").focus();*/
+      });
+  }
+
+  buscaCep ();
+
+
+
+
