04527521193ef009c037f5f36c7eb0aff1c10254
Jessany Kaline <jessany42@gmail.com>
2022-09-27 20:24:55 -0300

    página comprar
    Arrumando o fetch para consumir API

--- comprar/functions.js
+++ comprar/functions.js
@@ -1,71 +1,18 @@
-/*const cep = document.querySelector('#cep');
 
-cep.addEventListener("blur", (e) => {
-  let search = cep.value.replace("-", "");
-  console.log(search);
- 
-  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-  .then(response => {
-      response.json()
-      .then(data => {
-          console.log(data)
-          showData(data)
-      })
-  })
-  .catch(e => {
-      console.log("ERRO: "+e)
-  })
-})
-
-const showData = (result) => {
-  for (const campo in result){
-      if(document.querySelector("#endereco"+campo)){
-          document.querySelector("#endereco"+campo).value = result[campo];
-      }
-  }
-}*/
-
-/*const options = {
-   method: 'GET',
-   mode: 'no-cors',
-   cache: 'default'
-}*/
-
-
-
-
-/*function buscaCep(cep) {
-    fetch(`https://viacep.com.br/ws/${cep}/json`, options)
-      .then (response => response.json())
-      .then (dados => {
-        console.log(dados)
-  
-        document.querySelector("#endereco").value = dados.logradouro;
-        document.querySelector("#cidade").value = dados.localidade;
-        document.querySelector("#uf").value = dados.uf;
-  
-        document.querySelector("#txtNumero").focus();
-      });
-  }
-
-  buscaCep ();*/
 
   AOS.init();
 
-function validaEmail (email){
+  const cep = document.querySelector(#cep);
 
-   email = document.querySelector("#email").value;
+  fetch(`https://viacep.com.br/ws/${cep}/json`)
+  .then (response => response.json())
+  .then (dados => {
+    console.log(dados)
 
-    if(email.length < 3 || email.length > 18 ){
-      document.querySelector("#invalid-feedback").innerHTML = "Seu email não é válido";
-    }
-}
+    document.querySelector("#endereco").value = dados.logradouro;
+    document.querySelector("#cidade").value = dados.localidade;
+    document.querySelector("#uf").value = dados.uf;
 
-
-
-
-  validaEmail();
+    
+  });
   
-
-
-
