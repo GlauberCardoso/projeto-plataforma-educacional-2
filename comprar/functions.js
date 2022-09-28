fc00ab857535ea2d1bbf6618d84242f7fdec2dd9
Jessany Kaline <jessany42@gmail.com>
2022-09-28 13:49:11 -0300

    página comprar
    Atualização com consumo de API Viacep funcionando na página.

--- comprar/functions.js
+++ comprar/functions.js
@@ -1,18 +1,34 @@
 
+//Plugin de JS
 
   AOS.init();
 
-  const cep = document.querySelector(#cep);
 
-  fetch(`https://viacep.com.br/ws/${cep}/json`)
-  .then (response => response.json())
-  .then (dados => {
-    console.log(dados)
 
-    document.querySelector("#endereco").value = dados.logradouro;
-    document.querySelector("#cidade").value = dados.localidade;
-    document.querySelector("#uf").value = dados.uf;
+ 
+  //API para consultar endereço a partir do CEP
+ 
+  let input = document.querySelector("#cep");
 
-    
-  });
-  
+  input.addEventListener('keyup', (event) => {
+
+      let cep = document.querySelector("#cep");
+      let search = cep.value;
+        //console.log(search);
+
+      fetch(`https://viacep.com.br/ws/${search}/json`)
+    .then (response => response.json())
+    .then (dados => {
+      //console.log(dados)
+
+      document.querySelector("#endereco").value = dados.logradouro;
+      document.querySelector("#bairro").value = dados.bairro;
+      document.querySelector("#cidade").value = dados.localidade;
+      document.querySelector("#uf").value = dados.uf;
+
+
+    });
+    });
+
+
+     
